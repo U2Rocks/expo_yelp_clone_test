7b1bb0c7478d34a17fb8da7b3e46775e8fb201ff
Thomas Fry <59845454+U2Rocks@users.noreply.github.com>
2022-06-20 15:38:21 -0400

    removed older comments
    removed comments that were left in

--- src/api/yelp.js
+++ src/api/yelp.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 
-const yelp = "6jLz-xnFqCDMA9pF1cdSUqwx_fdEq7EapQSBjIzkmv9GbI92D5W2TU79rtkQn0KqynHalI2Q6VfyrYYgMzLumBl28TeJFAfgf69XigTsR9PYt_6McPoreHXpvWarYnYx"
+const yelp = "<YELP_API_KEY_HERE>"
 
 export default axios.create({
     baseURL: "https://api.yelp.com/v3/businesses",
